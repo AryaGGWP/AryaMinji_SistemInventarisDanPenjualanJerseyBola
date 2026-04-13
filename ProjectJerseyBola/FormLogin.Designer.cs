29d13d391af78cf52c3262b6a835576792f7e5f8
AryaGGWP <arya.lampung.2016@gmail.com>
2026-04-14 02:13:23 +0700

    membuat class untuk koneksi ke database

--- ProjectJerseyBola/FormLogin.Designer.cs
+++ ProjectJerseyBola/FormLogin.Designer.cs
@@ -109,7 +109,7 @@
             this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.label3.Font = new System.Drawing.Font("Rockwell Condensed", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(-3, 0);
+            this.label3.Location = new System.Drawing.Point(1, 5);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(595, 47);
             this.label3.TabIndex = 6;
---
+++ ProjectJerseyBola/Koneksi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Data.SqlClient; // Ini wajib buat manggil SQL Server
+
+namespace ProjectJerseyBola // Pastikan namespace ini sama kayak nama project lu
+{
+    class Koneksi
+    {
+        // Method buat nyambungin ke database
+        public SqlConnection GetConn()
+        {
+            // WARNING: Ganti tulisan di dalem kutip ini pakai Connection String lu sendiri!
+            SqlConnection conn = new SqlConnection(@"Data Source=IDEAPAD-ARYA\BANGDIO; Initial Catalog=DBAkademikADO; User ID=sa; Password=123; TrustServerCertificate=True");
+            return conn;
+        }
+    }
+}
